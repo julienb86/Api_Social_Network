ee84203edfe114118f7d07ec0262dde398b29d4b
julienb86 <julien.bouafia@gmail.com>
2021-06-05 11:39:16 +0200

    set up db with mongo

--- app.go
+++ app.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"log" // new
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"                              // new
+	config "github.com/julienb86/Api_Social_Network/Config" // new
 	routes "github.com/julienb86/Api_Social_Network/Routes"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(logger.New())
+
+	// dotenv
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// config db
+	config.ConnectDB()
 
 	SetUpRoutes(app)
 
 	// Listen on server 8000 and catch error if any
-	err := app.Listen(":8000")
+	err = app.Listen(":8000")
 
 	if err != nil {
 		panic(err)
--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.13
 
 require (
 	github.com/andybalholm/brotli v1.0.3 // indirect
+	github.com/devsmranjan/golang-fiber-basic-todo-app v0.0.0-20210321073654-3c80dc105968
 	github.com/gofiber/fiber/v2 v2.11.0
+	github.com/joho/godotenv v1.3.0
 	github.com/klauspost/compress v1.13.0 // indirect
+	go.mongodb.org/mongo-driver v1.5.3
 	golang.org/x/sys v0.0.0-20210603125802-9665404d3644 // indirect
 )
